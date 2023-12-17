74e804cbf79ecd074b78715715fcecc891aeff2e
Yannick <302867649@student.rocmondriaan.nl>
2023-12-17 18:21:39 +0100

    done

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
+// to get the div that display your info of you calculator
 const display = document.getElementById("display");
+// this function it the one reponsible for the printin to the display
+function toDisplay(input) {
+  // Check for multiple decimal points in the current value
+  if (input === '.' && display.value.includes('.')) {
+    return;
+  }
 
-function appendToDisplay(input) {
   display.value += input;
 }
-
+//this function is responsible for when you press the C button it will clear the display
 function clearDisplay() {
   display.value = "";
 }
 
 function calculate() {
   try {
-    display.value = eval(display.value);//eval function is like a mini calculator value in it self whne you type something in the calculator is will put then together for you
-  }
-  catch (error) {
+    const result = new Function('return ' + display.value)();
+    // Handle precision issues by rounding to a reasonable number of decimal places
+    display.value = Math.round(result * 1e12) / 1e12;
+  } catch (error) {
+    //if an error occurs it will display error
     display.value = "Error";
   }
 }
